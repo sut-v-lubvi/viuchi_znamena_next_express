eb359cf5a4109027f6f037456173e7609540c661
sut-v-lubvi <130555351+sut-v-lubvi@users.noreply.github.com>
2023-10-12 11:15:36 +0300

    commit24

--- client/src/app/settings/page.tsx
+++ client/src/app/settings/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
+import { useEffect } from "react";
 import { Container } from "./style";
 
 export default function SettingsPage() {
-  if (!localStorage.getItem("password")) {
-    const password: any = prompt("Введите пароль");
-    if (password === "yaplakal2000") {
-      localStorage.setItem("password", password);
-    } else {
-      location.reload();
+  useEffect(() => {
+    if (!localStorage.getItem("password")) {
+      const password: any = prompt("Введите пароль");
+      if (password === "yaplakal2000") {
+        localStorage.setItem("password", password);
+      } else {
+        location.reload();
+      }
     }
-  }
+  }, []);
 
   return <Container></Container>;
 }
