7180a6ff4a3d18c0691bb00e4cd472297a3d991f
sut-v-lubvi <130555351+sut-v-lubvi@users.noreply.github.com>
2023-10-18 15:15:21 +0300

    refactoring

--- client/src/widgets/NavMenu/NavMenu.tsx
+++ client/src/widgets/NavMenu/NavMenu.tsx
@@ -28,69 +28,74 @@ type LinksType = {
 };
 
 export default function NavMenu({ stateMenu, setStateMenu }: Props) {
+  const [data, setData] = useState<LinksType[]>();
   const { token, userId } = useAuth();
-  const dataLinks: LinksType[] = [
-    {
-      id: 1,
-      href: "/testing",
-      icon: (
-        <Kruk
-          dangerouslySetInnerHTML={{
-            __html: '<span class="red">â</span>[5',
-          }}
-        ></Kruk>
-      ),
-      text: "Выучи знамёна",
-    },
-    {
-      id: 2,
-      href: `/profile/${userId}`,
-      icon: <Prof />,
-      text: " Мой профиль",
-    },
-    {
-      id: 3,
-      href: "/rating",
-      icon: <KingIc />,
-      text: "Рейтинг",
-    },
-    {
-      id: 4,
-      href: "/documentation",
-      icon: <Doc />,
-      text: "Доукументация",
-    },
-    {
-      id: 5,
-      href: "/help",
-      icon: <Help />,
-      text: "Поддержка",
-    },
-    {
-      id: 6,
-      href: "/settings",
-      icon: <Settings />,
-      text: "Настройки",
-    },
-  ];
+  useEffect(() => {
+    setData([
+      {
+        id: 1,
+        href: "/testing",
+        icon: (
+          <Kruk
+            dangerouslySetInnerHTML={{
+              __html: '<span class="red">â</span>[5',
+            }}
+          ></Kruk>
+        ),
+        text: "Выучи знамёна",
+      },
+      {
+        id: 2,
+        href: `/profile/${userId}`,
+        icon: <Prof />,
+        text: " Мой профиль",
+      },
+      {
+        id: 3,
+        href: "/rating",
+        icon: <KingIc />,
+        text: "Рейтинг",
+      },
+      {
+        id: 4,
+        href: "/documentation",
+        icon: <Doc />,
+        text: "Доукументация",
+      },
+      {
+        id: 5,
+        href: "/help",
+        icon: <Help />,
+        text: "Поддержка",
+      },
+      {
+        id: 6,
+        href: "/settings",
+        icon: <Settings />,
+        text: "Настройки",
+      },
+    ]);
+  }, [userId]);
+
   return (
     <Menu $stateMenu={stateMenu}>
       <Container>
         <FlexContainer>
-          {dataLinks.map((e) => {
-            return (
-              <Links
-                key={e.id}
-                onClick={() => {
-                  setStateMenu(!stateMenu);
-                }}
-                href={e.href}
-              >
-                {e.icon}
-                {e.text}
-              </Links>
-            );
-          })}
+          {data &&
+            data.map((e) => {
+              return (
+                <Links
+                  key={e.id}
+                  onClick={() => {
+                    setStateMenu(!stateMenu);
+                  }}
+                  href={e.href}
+                >
+                  {e.icon}
+                  {e.text}
+                </Links>
+              );
+            })}
         </FlexContainer>
       </Container>
     </Menu>
